f52bbabfea803e495eb6764bf29b644ff808cd06
Amansingh2oo3 <Amansinghhhh2oo3@gmail.com>
2025-06-12 23:36:43 +0530

    First Commit

---
+++ bridgelabz/src/bridgelabz/qthree.java
@@ -0,0 +1,11 @@
+package bridgelabz;
+
+
+public class qthree {
+	public static void main(String[] args) {
+		double distance = 10.8;
+		double miles = distance * 1.6;
+		System.out.println("The distance " + distance + " distance in miles is " + miles);
+	}
+	
+}
